cf5652a019e5a9c6e41e44a259fc3dce35b5d8d5
Muhammad <ranaahmad131@gmail.com>
2023-12-09 23:55:32 +0500

    Working on navigation

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">React Pizza App</Link>
+      <p>Muhammad</p>
+    </header>
+  );
+}
+
+export default Header;
